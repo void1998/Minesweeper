a2536ea29aa6cd69658537822135960160059200
mahmoud.tr <mamhoudtrro@gmail.com>
2018-11-02 23:18:20 +0200

    Console player input

--- Programming3.1/src/programming3/pkg1/Player.java
+++ Programming3.1/src/programming3/pkg1/Player.java
@@ -1,17 +1,44 @@
 package programming3.pkg1;
 public abstract class Player {
 
-    protected String name;
-    protected Score currentScore;
+    private String name;
+    private Score currentScore;
+    private PlayerStatue currentStatue;
     
     Player()
     {
         this.name = "Player";
         this.currentScore = new NumiricScore();
+        this.currentScore = new NumiricScore();
+        this.currentStatue = new PlayerStatue();
+    }
+    Player(Score score,PlayerStatue statue)
+    {
+        this.currentScore = score;
+        this.currentStatue = statue;
     }
 
     public Score getCurrentScore() {
         return currentScore;
+    }
+    public void SetCurrentScore(Score score) {
+        this.currentScore = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayerStatue getCurrentStatue() {
+        return currentStatue;
+    }
+
+    public void setCurrentStatue(PlayerStatue currentStatue) {
+        this.currentStatue = currentStatue;
     }
     
     public abstract PlayerMove GetPlayerMove();
