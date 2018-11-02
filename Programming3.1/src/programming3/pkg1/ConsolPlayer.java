c21230dbaeba7b052ec7a3a32d171ecdc9334a32
mahmoud.tr <mamhoudtrro@gmail.com>
2018-11-02 22:13:00 +0200

    Console player input

--- Programming3.1/src/programming3/pkg1/ConsolPlayer.java
+++ Programming3.1/src/programming3/pkg1/ConsolPlayer.java
@@ -2,39 +2,21 @@ package programming3.pkg1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-/**
- *
- * @author void
- */
-public abstract class Player {
-
-    protected String name;
-    protected Score currentScore;
-    
-    Player()
-    {
-        this.name = "Player";
-        this.Score = new NormalScore();
-    }
-    public abstract PlayerMove GetPlayerMove();
-    
-}
-
-public class ConsolePlayer extends Player {
+public class ConsolPlayer extends Player {
+    @Override
     public PlayerMove GetPlayerMove()
     {
-        PlayerMove currentMove = new PlayerMove;
+        PlayerMove currentMove = new PlayerMove();
         Square square = new Square();
         MoveType currentType = new MoveType();
         BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
         try {
             System.out.println("to mark square please enter 1, to reveal square please enter 2 ");
             String Insert = reader.readLine();
-            int insert = Integer.parseInt(Insert);
-            currentType.setMoveType(insert);
+            currentType.setType(Insert);
         } catch (IOException ex) {
             Logger.getLogger(ConsolePlayer.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -55,9 +37,9 @@ public class ConsolePlayer extends Player {
         catch (IOException ex) {
              Logger.getLogger(ConsolePlayer.class.getName()).log(Level.SEVERE, null, ex);
             }
-        currentMove.setPlayer(this.name);
+        currentMove.setPlayer(this);
         currentMove.setSquare(square);
-        currentMove.setMoveType(currentType);
+        currentMove.setMove(currentType);
         return currentMove;
     }
 }
