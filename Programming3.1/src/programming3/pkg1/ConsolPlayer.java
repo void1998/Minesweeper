19abec668d882deee4876c779d522f74bd56811c
CEC <CEC@DESKTOP-LOUAY>
2018-11-07 17:51:20 +0200

    new things

--- Programming3.1/src/programming3/pkg1/ConsolPlayer.java
+++ Programming3.1/src/programming3/pkg1/ConsolPlayer.java
@@ -13,30 +13,58 @@ public class ConsolPlayer extends Player {
         Square square = new Square();
         MoveType currentType = new MoveType();
         BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
-        try {
-            System.out.println("to mark square please enter 1, to reveal square please enter 2 ");
-            String Insert = reader.readLine();
-            currentType.setType(Insert);
-        } catch (IOException ex) {
-            Logger.getLogger(ConsolePlayer.class.getName()).log(Level.SEVERE, null, ex);
-        }
+       while(true)
+           {
+             try {
+                  System.out.println("to mark square please enter 1, to reveal square please enter 2 ");
+                  String Insert = reader.readLine();
+                  if(Insert.equals("1") && Insert.equals("2"))
+                    throw new IllegalGameMove("illegal move!");
+                  currentType.setType(Insert);
+                  break;
+            } catch (IllegalGameMove ex) {
+            System.out.println(ex);
+        }   catch (IOException ex) {
+                Logger.getLogger(ConsolPlayer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+          }
+       
+       
+       while(true)
+       {
         try {
             System.out.println("Please enter the letter of the column");
             String Insert = reader.readLine();
             int insert = Integer.parseInt(Insert);
+             if(insert>19 && insert<1)
+                throw new IllegalSquareName("illegal square name!");
             square.setX(insert);
-        } catch (IOException ex) {
+            break;
+        }  
+        catch (IllegalSquareName ex) {
+             System.out.println(ex);  
+        }catch (IOException ex) {
             Logger.getLogger(ConsolePlayer.class.getName()).log(Level.SEVERE, null, ex);
         }
+       }
+       
+       while(true)
+       {
         try {
             System.out.println("please enter the number of the row");
             String Insert = reader.readLine();
             int insert = Integer.parseInt(Insert);
+            if(insert>19 && insert<1)
+                throw new IllegalSquareName("illegal square name!");
             square.setY(insert);
+            break;
             } 
+        catch (IllegalSquareName ex) {
+             System.out.println(ex);            }
         catch (IOException ex) {
              Logger.getLogger(ConsolePlayer.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            } 
+       }
         currentMove.setPlayer(this);
         currentMove.setSquare(square);
         currentMove.setMove(currentType);
--- Programming3.1/src/programming3/pkg1/GameException.java
+++ Programming3.1/src/programming3/pkg1/GameException.java
@@ -1,9 +1,11 @@
 package programming3.pkg1;
 
+
 public abstract class GameException extends Exception{
 
     public GameException(String string) {
         super(string);
     }
+    
     public abstract String getException();
 }
